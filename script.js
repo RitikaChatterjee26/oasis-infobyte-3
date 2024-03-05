06f381afa10333d551245c6cda7b4c24271ff5e2
RitikaChatterjee26 <155581960+RitikaChatterjee26@users.noreply.github.com>
2024-03-05 14:36:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,64 @@
+(function(){
+    const fonts = ["cursive" ,"sans-serif" ,"serif","monospace"];
+    let captchaValue ="";
+    function generateCaptcha(){
+        let value=btoa(Math.random()*1000000000);
+        value=value.substr(0, 5+Math.random()*5);
+        captchaValue=value;
+
+    }
+    function setCaptcha(){
+       let html= captchaValue.split("").map((char)=>{
+            const rotate= -20 + Math.trunc(Math.random()*30);
+            const font= Math.trunc(Math.random()*fonts.length);
+            return `<span
+            Style="
+            transform:rotate(${rotate}deg);
+            font-family:${fonts[font]}
+            "
+            >${char}</span>`;
+
+        }).join("");
+        document.querySelector(".signin-form .captcha .preview").innerHTML=html;
+    }
+    function initCaptcha(){
+        document.querySelector(".signin-form .captcha .captcha-refresh").addEventListener("click",function(){
+        generateCaptcha();
+        setCaptcha();
+        });
+        generateCaptcha();
+        setCaptcha();
+    }
+    initCaptcha();
+
+
+        document.querySelector(".signin-form #login-btn").addEventListener("click",function(){
+            let inputCaptchaValue=document.querySelector(".signin-form  .captcha-input").value;
+            if (inputCaptchaValue===captchaValue){
+                swal("","Logging In!","success");
+            }else{
+                swal("Invalid captcha");
+            }
+        });
+    })();
+            
+            
+        
+
+
+        
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
